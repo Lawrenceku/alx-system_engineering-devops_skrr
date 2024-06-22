30932844018e9bc589d22cc506bafcfc0b0760b5
Lawrence <lawrencekunmilola@gmail.com>
2024-06-22 23:47:29 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
-"""
-number of subscribers for a given subreddit
-"""
-
-from requests import get, exceptions
+"""Function to query subscribers on a given Reddit subreddit."""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    If an invalid subreddit is given, return 0.
-    """
-    if subreddit is None or not isinstance(subreddit, str):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-
-    user_agent = {'User-agent': 'Mozilla/5.0'}
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-
-    try:
-        response = get(url, headers=user_agent, allow_redirects=False)
-        # Check for successful response
-        if response.status_code != 200:
-            return 0
-        results = response.json()
-        return results.get('data', {}).get('subscribers', 0)
-    except (exceptions.RequestException, ValueError):
-        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
